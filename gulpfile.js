00d2c04eb8e8a8120191a0da08d61611f05ba48c
Marketa Dragounova <marketa.dragounova@gmail.com>
2019-02-14 16:23:38 -0800

    Gulp setup properly for compiling and watching

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync').create();
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
 
-// Static server
-gulp.task('sync', function() {
+// Static Server + watching scss/html files
+gulp.task('serve', function() {
+
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
 
-    gulp.watch('css/*').on('change', browserSync.reload);
-    gulp.watch('index.html').on('change', browserSync.reload);
+    gulp.watch("sass/**/*.scss", gulp.series('sass'));
+    gulp.watch('*.html').on('change', browserSync.reload);
+    gulp.watch('css/style.css').on('change', browserSync.reload);
 });
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src('sass/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest("css/"));
+});
+
+gulp.task('default', gulp.series('serve'));
